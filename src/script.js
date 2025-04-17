d120cd7be17f926a2ab3f3955a057f24d5b9845c
cynkzn <cynkazan@gmail.com>
2025-04-17 20:33:56 +1000

    added my current location

--- src/script.js
+++ src/script.js
@@ -37,6 +37,9 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimezone = event.target.value;
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
   let cityName = cityTimezone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimezone);
   let cityElement = document.querySelector("#cities");
